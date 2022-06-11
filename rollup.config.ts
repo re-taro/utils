6868db4e9db24e41980964f6ffae5fefdf3e63f6
Re-taro <aminokacho@gmail.com>
2022-06-11 21:13:28 +0900

    🎉 init

---
+++ rollup.config.ts
@@ -0,0 +1,50 @@
+import alias from '@rollup/plugin-alias'
+import commonjs from '@rollup/plugin-commonjs'
+import json from '@rollup/plugin-json'
+import resolve from '@rollup/plugin-node-resolve'
+import dts from 'rollup-plugin-dts'
+import esbuild from 'rollup-plugin-esbuild'
+
+const entries = ['src/index.ts']
+
+const plugins = [
+  alias({
+    // eslint-disable-next-line prefer-named-capture-group, require-unicode-regexp
+    entries: [{ find: /^node:(.+)$/, replacement: '$1' }]
+  }),
+  resolve({
+    preferBuiltins: true
+  }),
+  json(),
+  commonjs(),
+  esbuild({
+    target: 'node14'
+  })
+]
+
+export default [
+  ...entries.map((input) => ({
+    external: [],
+    input,
+    output: [
+      {
+        file: input.replace('src/', 'dist/').replace('.ts', '.mjs'),
+        format: 'esm'
+      },
+      {
+        file: input.replace('src/', 'dist/').replace('.ts', '.cjs'),
+        format: 'cjs'
+      }
+    ],
+    plugins
+  })),
+  ...entries.map((input) => ({
+    external: [],
+    input,
+    output: {
+      file: input.replace('src/', '').replace('.ts', '.d.ts'),
+      format: 'esm'
+    },
+    plugins: [dts({ respectExternal: true })]
+  }))
+]
